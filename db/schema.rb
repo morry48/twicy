a1d76aef278c7824716f2c5e02b617c795f2cabf
morry48 <yyyyuuuuuuuu0408@gmail.com>
2019-03-27 12:41:58 +0900

    tweet-new-view
    Tweet-newの機能実装完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190327030836) do
+
+  create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "content",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
 end
